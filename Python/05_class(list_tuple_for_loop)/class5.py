37ace9b40e0f7835caab4dbbc7e93cdb551cc2e8
abdulsami12 <abdulsami9718@gmail.com>
2024-01-15 00:07:22 +0500

    work

---
+++ Python/05_class(list_tuple_for_loop)/class5.py
@@ -0,0 +1,11 @@
+name:list[str] = ["sami" , "Taha","Faiq"]
+for name in name:
+    print(name)
+    
+    
+    # Tuple
+data_base:list[tuple[str,str]] = [("sami","123"),(
+    "Taha","456"),("faiq","123")]
+
+for rew in data_base:
+    print(rew)    
